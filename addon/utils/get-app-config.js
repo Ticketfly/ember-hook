43b968b0bd8e7f8aa952c42e1355e55de7a5f1dd
null null null <null@revolution.cheap>
2016-02-16 12:46:29 -0800

    add HookMixin

--- addon/utils/get-app-config.js
+++
@@ -1,7 +0,0 @@
-export default function getAppConfig() {
-  const configName = Object.keys(requirejs.entries).filter((entry) => {
-    return entry.match(/\/config\/environment/);
-  })[0];
-
-  return requirejs(configName).default;
-}
--- tests/dummy/app/components/test-component.js
+++ tests/dummy/app/components/test-component.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
+import { HookMixin } from 'ember-hook';
 
 const { Component } = Ember;
 
-export default Component.extend();
+export default Component.extend(HookMixin);
