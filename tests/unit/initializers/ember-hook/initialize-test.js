ecc817f8e3ec6279ab64c0bf150422dd2b8fb809
null null null <null@revolution.cheap>
2016-08-04 16:46:45 -0700

    fix failing beta test

--- tests/unit/initializers/ember-hook/initialize-test.js
+++ tests/unit/initializers/ember-hook/initialize-test.js
@@ -23,6 +23,6 @@ test('applies the `HookMixin` to `Component`', function(assert) {
   const { Component } = Ember;
   const component = Component.create({ hook: 'foo' });
 
-  assert.deepEqual(component.get('attributeBindings'), ['ariaRole:role', '_hookName:data-test'], 'adds _hookName to the attributeBindings');
+  assert.ok(component.get('attributeBindings').indexOf('_hookName:data-test') > -1, 'adds _hookName to the attributeBindings');
   assert.equal(component.get('_hookName'), `foo${delimiter}`, 'adds the _hookName computed');
 });
