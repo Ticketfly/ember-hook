bf66d5c965484c16009a7949c431f742273added
Michael Carroll <juwara0@users.noreply.github.com>
2016-08-04 17:32:02 -0500

    added development environment to the default and a test to verify

--- addon/utils/return-when-testing.js
+++ addon/utils/return-when-testing.js
@@ -5,7 +5,7 @@ const { get } = Ember;
 export default function returnWhenTesting(config, value) {
   const enabled = get(config, 'emberHook.enabled');
 
-  if (typeof enabled === 'boolean' ? enabled : config.environment === 'test') {
+  if (typeof enabled === 'boolean' ? enabled : config.environment === 'test' || config.environment === 'development') {
     return value;
   }
 }
