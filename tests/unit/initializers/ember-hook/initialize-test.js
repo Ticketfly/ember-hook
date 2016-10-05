7b301de52a9b7d3dfb64c5d57093f38382b4f7b0
null null null <null@affinitygamescollective.org>
2016-10-05 15:55:26 -0700

    update ember-get-config

--- tests/unit/initializers/ember-hook/initialize-test.js
+++ tests/unit/initializers/ember-hook/initialize-test.js
@@ -20,9 +20,7 @@ test('applies the `HookMixin` to `Component`', function(assert) {
 
   initialize();
 
-  const component = Ember.Component.proto();
-
-  component.setProperties({ hook: 'foo', hookQualifiers: { index: 3 } });
+  const component = Ember.Component.create({ hook: 'foo', hookQualifiers: { index: 3 } });
 
   assert.ok(component.get('attributeBindings').indexOf('_hookName:data-test') > -1, 'adds _hookName to the attributeBindings');
   assert.equal(component.get('_hookName'), `foo${delimiter}index=3${delimiter}`, 'adds the _hookName computed');
